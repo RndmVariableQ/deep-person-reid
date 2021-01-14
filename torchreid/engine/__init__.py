1a531a44543abe6769208232a8c585394f006627
RndmVariableQ <63947190+RndmVariableQ@users.noreply.github.com>
2021-01-14 19:39:47 +0800

    init

--- torchreid/engine/__init__.py
+++ torchreid/engine/__init__.py
@@ -2,4 +2,5 @@ from __future__ import print_function, absolute_import
 
 from .image import ImageSoftmaxEngine, ImageTripletEngine
 from .video import VideoSoftmaxEngine, VideoTripletEngine
+from .video.oim import VideoOIMEngine
 from .engine import Engine
